e9d532cfefe41c4de98a4ce50fcdb3cc8ad858b2
Fu-Ming <716585+sary357@users.noreply.github.com>
2022-03-11 18:40:27 +0800

    remove default node pool

--- IaC/gke/gke.tf
+++ IaC/gke/gke.tf
@@ -29,17 +29,17 @@ resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-${var.my_name}-gke"
   location = var.zone
   min_master_version = var.gke_version
-  node_version = var.gke_version
+  # node_version = var.gke_version
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
   initial_node_count       = 1
-  node_config {
+/*  node_config {
     service_account = var.service_account
-    machine_type = "n1-standard-2"
-    image_type   = "COS" # GKE does not support COS
-    # image_type = "COS_CONTAINERD"
+    machine_type = "e2-micro"
+    # image_type   = "COS" # GKE does not support COS
+    image_type = "COS_CONTAINERD"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
@@ -47,7 +47,7 @@ resource "google_container_cluster" "primary" {
     labels = {
       foo = "qqq"
     }
-  }
+  } */
 
   release_channel {
     channel = "RAPID"
@@ -67,6 +67,39 @@ resource "google_container_node_pool" "primary_nodes" {
   location   = var.zone
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
+
+  node_config {
+    service_account = var.service_account
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+      foo = "ppp"
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-2"
+    # image_type   = "COS" # GKE does not support COS
+    image_type   = "COS_CONTAINERD"
+    tags         = ["gke-node", "${var.project_id}-${var.my_name}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+  timeouts {
+    create = "10m"
+    update = "20m"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes2" {
+  name       = "${var.project_id}-${var.my_name}-npl2"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
 
   node_config {
     service_account = var.service_account
